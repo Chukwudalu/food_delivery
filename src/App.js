7ffe7a4c78a7a53642a686cc7905aea4c8044f4f
Chukwudalu <jerryolisa@gmail.com>
2022-04-06 15:12:07 -0700

    worked on the banner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import { AccountBalanceWalletRounded, HomeRounded, SummarizeRounded, Chat, Favorite, Settings } from '@mui/icons-material';
+import { useEffect } from 'react';
 import './App.css';
+import BannerName from './Components/BannerName';
 import Header from './Components/Header';
 import MenuContainer from './Components/MenuContainer'
+// import bannerImg from './assets/deliveryman.jpg'
 
 function App() {
+
+  useEffect(() => {
+    const menuLi = document.querySelectorAll('#menu li');
+    function setMenuActive(){
+      console.log(this)
+      menuLi.forEach(n => n.classList.remove('active'));
+      this.classList.add('active')
+    }
+    menuLi.forEach(n => n.addEventListener('click', setMenuActive))
+  }, [])
+
   return (
     <div className="App">
       {/* Header Section */}
       <Header/>
       {/* Main Container */}
       <main>
-        <div className="mainContainer"></div>
+        <div className="mainContainer">
+          <div className='banner'>
+            <BannerName name={"Jeremiah"} discount={20} link={"#"}/>
+            {/* <img
+              src={bannerImg}
+              alt=''
+              className='deliveryPic'
+            /> */}
+          </div>
+        </div>
         <div className="rightMenu"></div>
       </main>
       {/* Bottom Menu*/}
@@ -29,7 +52,7 @@ function App() {
           {/* prettier-ignore */}
           <MenuContainer link={'#'} icon={<Settings/>}/>
 
-          <div className=".indicator"></div>
+          <div className="indicator"></div>
         </ul>
       </div>
     </div>
